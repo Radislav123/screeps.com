36175bb4b8e5cebf0779c97ead366a2bf4a39edc
Vlasov R. V <radislavvlasov@gmail.com>
2022-03-11 23:34:38 +0300

    cleanup the project

---
+++ source/creeps/base.py
@@ -0,0 +1,22 @@
+from screeps_api import *
+
+# noinspection DuplicatedCode
+if True:
+    __pragma__('noalias', 'name')
+    __pragma__('noalias', 'undefined')
+    __pragma__('noalias', 'Infinity')
+    __pragma__('noalias', 'keys')
+    __pragma__('noalias', 'get')
+    __pragma__('noalias', 'set')
+    __pragma__('noalias', 'type')
+    __pragma__('noalias', 'update')
+
+
+class BaseCreep:
+    """Базовый крип, не должен использоваться напрямую."""
+
+    body = []
+
+    @classmethod
+    def spawn(cls, structure_spawn: StructureSpawn):
+        structure_spawn.spawnCreep(cls.body, "Harvester_{}".format(Game.time))
